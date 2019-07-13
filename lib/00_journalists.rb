364f74630fc11b69ccbc5eab7a693aec8ba5a8c2
Fast Fabien <fastfabien56@gmail.com>
2019-07-13 14:07:44 +0300

    ajouts des projets

---
+++ lib/00_journalists.rb
@@ -0,0 +1,184 @@
+
+#initialisation du tableau
+def array_init
+	journalist = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+	return journalist
+end
+
+#number of journalist
+def number_journalist
+	puts array_init.length
+end
+
+#number of pseudo who start with num
+def pseudo_num(tab_name)
+	number = 0
+	tab = tab_name.length
+
+	tab.times do |i|
+		vao = tab_name[i]
+		handle = 0
+		vao.length.times do |j|
+			case vao[j]
+			when "0".."9"
+				handle = 1
+			end
+		end
+		if handle == 1
+			number += 1
+		end
+	end
+
+puts number
+end
+
+#number of the journalist who pseudo content aude
+def number_aude(tab_name)
+	number = 0
+	tab = tab_name.length
+
+	tab.times do |i|
+		vao = tab_name[i]
+		handle = 0
+		vao.length.times do |j|
+			if vao[j] == "a" || vao[j] == "A"
+				if vao[j+1] == "u"
+					if vao[j+2] == "d"
+						if vao[j+3] == "e"
+							handle = 1
+						end
+					end
+				end
+			end
+
+			
+		end
+		if handle == 1
+			number += 1
+		end
+	end
+	puts number
+end
+
+#number of pseudo who start with majuscule after @
+def start_majuscule(tab_name)
+	number = 0
+	tab = tab_name.length
+
+	tab.times do |i|
+		vao = tab_name[i]
+		handle = 0
+		vao.length.times do |j|
+			case vao[i][1]
+			when "A".."Z"
+				handle = 1
+			end
+		end
+		if handle == 1
+			number += 1
+		end
+	end
+ puts number
+end
+
+#n
+def start_majuscule_after(tab_name)
+	number = 0
+	tab = tab_name.length
+
+	tab.times do |i|
+		vao = tab_name[i][1]
+		handle = 0
+		vao.length.times do |j|
+			case vao[j]
+			when "A".."Z"
+				handle = 1
+			end
+		end
+		if handle == 1
+			number += 1
+		end
+	end
+ puts number
+end
+
+def majuscule(tab_name)
+	number = 0
+	tab = tab_name.length
+
+	tab.times do |i|
+		vao = tab_name[i]
+		handle = 0
+		vao.length.times do |j|
+			case vao[j]
+			when "A".."Z"
+				handle = 1
+			end
+		end
+		if handle == 1
+			number += 1
+		end
+	end
+ puts number
+end
+
+
+def underscore_number(tab_name)
+	number = 0
+	tab = tab_name.length
+
+	tab.times do |i|
+		vao = tab_name[i]
+		handle = 0
+		vao.length.times do |j|
+			case vao[j]
+			when "_"
+				handle = 1
+			end
+		end
+		if handle == 1
+			number += 1
+		end
+	end
+
+puts number
+end
+
+def tri_alphabetic(tab_name)
+	puts tab_name.sort
+end
+
+#50 handle le plus courts
+def pseudo_short(tab_name)
+	tab = tab_name.length - 1
+	essai = 0
+	while essai == 0
+		essai = 1
+		for i in 0...tab
+			if tab_name[i].length > tab_name[i+1].length
+			tab_name[i],tab_name[i+1] = tab_name[i+1],tab_name[i]
+			essai = 0
+			end
+		end
+	end
+	puts tab_name[0..50]
+end
+
+#position de l'array @epenser
+def epenser_position
+	puts array_init.index("@epenser")
+end
+
+def viewResult
+	puts "Bonjours !!!!"
+	puts "Que voulez vous savoir sur notre organisation ?"
+	puts "1. Le nombre de nos journalistes\n2. Le nombre de pseudo qui contien un nombre\n3. Le nombre des pseudos qui contient Aude\n4. Ceux qui commence par une majuscule\n5. Combien contiennet une majuscule ?\n6. Combien contient un underscore ?\n7. Les listes par ordre alphabetique\n8. Les 50 plus cours pseudo\n9. La position de @epenser \n\n"
+	choix = gets.chomp
+
+	case ch
+	when "1"
+		
+		
+	end
+end
+viewResult
